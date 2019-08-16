42ffb651358ddb1369c83a91e7ba8483d9bca26e
akhilcm <akhilcm777@gmail.com>
2019-08-16 14:45:25 +0530

    library update

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ app.set('view engine','ejs');
 app.use(Express.static(__dirname+"/public"));
 
 app.get('/',(req,res)=>{
-    res.render('index');
+    res.render('index',{nav:[{link:'/books',title:'books'},{
+        link:'/author',author:'author'}],title:'Library'});
+
 });
 app.listen(3000);
