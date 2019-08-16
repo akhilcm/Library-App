6d28e876e6ad523e05a5594b74d6e4f27afa93b0
akhilcm <akhilcm777@gmail.com>
2019-08-16 14:09:33 +0530

    library

---
+++ app.js
@@ -0,0 +1,9 @@
+const Express = require('express');
+var app=new Express();
+app.set('view engine','ejs');
+app.use(Express.static(__dirname+"/public"));
+
+app.get('/',(req,res)=>{
+    res.render('index');
+});
+app.listen(3000);
